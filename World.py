18eb74a047da1e47db6e73e1b36d2549c19e03c9
Rachel <green_chul@hotmail.com>
2021-08-04 19:02:46 +0100

    WIP

---
+++ World.py
@@ -0,0 +1,101 @@
+import secrets
+import random
+import names
+import threading
+import time
+
+
+
+class World:
+   
+    def __init__(self):
+        self.name = f"{names.get_last_name()}-{secrets.token_urlsafe(3)}"
+        self.colour = (random.randrange(0,255), random.randrange(0,255),random.randrange(0,255))
+        self.number_of_weather_balloons = random.randrange(5,20)
+        self.weather_balloons = []
+        self.maximum_z = 100
+        self.maximum_x = 300
+        self.maximum_y = 600
+
+        for _balloon_index in range(self.number_of_weather_balloons):
+            balloon = Weather_balloon(maximum_z = self.maximum_z, maximum_x= self.maximum_x, maximum_y = self.maximum_y)
+            self.weather_balloons.append(balloon)
+
+    def get_world_data_for_api(self):
+        world_data = {}
+        world_data["name"] = self.name
+        world_data["colour"] = self.colour
+        world_data["number_of_weather_balloons"] = self.number_of_weather_balloons
+        world_data["balloons"] = []
+        
+        for weather_balloon in self.weather_balloons:
+            world_data["balloons"].append(weather_balloon.get_data())
+
+        return world_data
+
+    """
+    Balloon class
+
+    """
+class Weather_balloon:
+    def __init__(self, maximum_x = 300, maximum_z = 100, maximum_y = 600):
+        # self.maximum_altitude = maximum_altitude
+        self.maximum_y = maximum_y
+        self.maximum_x = maximum_x
+        self.maximum_z = maximum_z
+        self.location = [random.randrange(-self.maximum_x, self.maximum_x), -self.maximum_y/2, 0]
+        self.balloon_colour = (random.randrange(0,255),random.randrange(0,255),random.randrange(0,255))
+        self.balloon_name = secrets.token_urlsafe(2)
+        self.balloon_temperature = random.randrange(-25, 50)
+        self.increase_altitude()
+
+
+    def get_data(self):
+        data = {}
+        data["location"] = self.location
+        data["balloon_colour"] = self.balloon_colour
+        data["balloon_name"] = self.balloon_name
+        data["balloon_temperature"] = self.balloon_temperature
+        return data
+
+
+    # def increase_height(self):
+    #     self.height = 0
+
+
+    def increase_altitude(self):
+        
+        # now start moving anything that ready to be moved
+        # Do the work in another thread to keep it fast
+        kwargs = {}
+        args = []
+        threaded_function = self.increase_altitude_threaded
+        thread = threading.Thread(
+        target=threaded_function, args=args, kwargs=kwargs
+        )
+        thread.start()
+
+    def increase_altitude_threaded(self):
+        
+        while True:
+            self.location[1] = self.location[1] + random.randrange(1, 6)
+            
+            time.sleep(1)
+
+
+
+    
+
+
+if __name__ == "__main__":
+    my_new_word = World()
+    # print(my_new_word.name)
+
+    # while True:
+    #     print("about to sleep")
+    #     time.sleep(0.5)
+    
+
+
+
+            
--- routes.py
+++ routes.py
@@ -2,8 +2,9 @@ from flask import Flask, request, render_template, jsonify
 import os
 import random
 import json
-from balloons import World
-from balloons import Weather_balloon
+from World import World 
+world = World()
+# from balloons import Weather_balloon
 
  
 basedir = os.path.abspath(os.path.dirname(__file__))    
@@ -14,58 +15,31 @@ app = Flask(__name__)
 # to the browser 
 app.config["SECRET_KEY"] = "We just need a secret here: I like yellow."
 
-@app.route("/")
+@app.route("/") 
 def index(): 
     rendered_html = render_template("index.html")
     return rendered_html
-   
-@app.route("/balloon_world") 
-def balloon_world():
-    number_of_balloons = int(random.random() * 10) 
-
-    balloons = [] 
-
-    # create world 
-    created_world = World()   
-
-    #create balloons   
-
-    for _balloon in range(number_of_balloons):
-        balloon = {}
-        created_balloon = Weather_balloon()
-        print(created_balloon.location) 
-        balloon["location"] = created_balloon.location
-        print(created_balloon.balloon_colour)
-        balloon["colour"] = created_balloon.balloon_colour
-        print(created_balloon.balloon_name)
-        balloon["name"] = created_balloon.balloon_name 
-
-        print(balloon)  
-        balloons.append(balloon)       
-
-    created_world_name = created_world.name 
-    created_world_colour = created_world.colour          
-
     
-   
-    balloons_as_json = json.dumps(balloons)           
-      
-    
+@app.route("/api_balloon_world") 
+def api_balloon_world():
+    # number_of_balloons = int(random.random() * 10)       
 
-    return jsonify(dict(number_of_balloons = number_of_balloons, created_world_name = created_world_name, created_world_colour = created_world_colour, balloons_as_json = balloons_as_json))
-                        
+    return jsonify(world.get_world_data_for_api())  
                         
       
-
+@app.route("/test")
+def test():
+    rendered_html = render_template("test.html")
+    return rendered_html
 
 """not sure if need this route""" 
-@app.route("/display_balloon_world")
+@app.route("/display_balloon_world") 
 def display_balloon_world(): 
-    rendered_html = render_template("display_balloon_world.html")   
+    rendered_html = render_template("display_balloon_world.html")         
     return rendered_html                 
    
     
 
-
+ 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0") 
